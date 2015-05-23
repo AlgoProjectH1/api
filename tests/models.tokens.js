8bc4387aa475939c08b11dbb93ddac9b97e2d421
Romain Quilliot <romain.addweb@gmail.com>
2015-05-23 23:55:40 +0200

    Add decode tokens tests

--- tests/models.tokens.js
+++ tests/models.tokens.js
@@ -1,11 +1,18 @@
 var assert = require('assert');
 var tokens = require('../src/models/tokens.js');
 
-describe('Tokens', function(){
-    describe('#format()', function(){
+describe('Tokens', function() {
+    describe('#format()', function() {
         it('should return {token}:{user} string', function() {
             var token = {token: "1234abcd", user: 1};
             assert.equal("1234abcd:1", tokens.format(token.token, token.user));
+        })
+    })
+
+    describe('#decode()', function() {
+        it('should return {token, user, device} object', function () {
+            var token = "1234abcd:1:computer";
+            assert.equal(JSON.stringify({token:"1234abcd",user:"1",device:"computer"}), JSON.stringify(tokens.decode(token)));
         })
     })
 })
