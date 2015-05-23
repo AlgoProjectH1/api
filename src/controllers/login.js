f5eead14d771c89f24cf352f82ebf7c9092ff7a8
Romain Quilliot <romain.addweb@gmail.com>
2015-05-23 23:13:08 +0200

    Add login route

---
+++ src/controllers/login.js
@@ -0,0 +1,57 @@
+var Login = {
+    res: "",
+    models: ""
+};
+
+Login.index = function (req, res) {
+    var pseudo = req.body.pseudo ? req.body.pseudo : false;
+    var password = req.body.password ? req.body.password : false;
+
+    Login.res = res;
+    Login.models = req.app.get('models');
+
+    if (!pseudo) {
+        res.send(JSON.stringify({
+            error: true,
+            message: "Merci d'indiquer un pseudo"
+        }));
+
+        return false;
+    }
+
+    if (!password) {
+        res.send(JSON.stringify({
+            error: true,
+            message: "Merci d'indiquer un mot de passe"
+        }));
+
+        return false;
+    }
+
+    Login.models.users.login({
+        pseudo: pseudo,
+        password: password,
+        device: 'computer'
+    }, Login.failure, Login.success);
+
+};
+
+Login.failure = function (errorMessage) {
+    Login.res.send(JSON.stringify({
+        error: true,
+        message: errorMessage
+    }));
+};
+
+Login.success = function (token) {
+    var decodedToken = Login.models.tokens.decode(token);
+    Login.models.tokens.updateLastUse(decodedToken.token, decodedToken.user);
+
+    Login.res.send(JSON.stringify({
+        error: false,
+        token: token
+    }));
+};
+
+
+module.exports = Login.index;
